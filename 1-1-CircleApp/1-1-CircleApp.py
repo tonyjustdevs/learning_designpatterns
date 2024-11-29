df9db45a9c8e4fcc0270e75bfdfc492e458593a5
tonyjustdevs <tonyjustdevs@gmail.com>
2024-11-29 18:33:29 +0700

    setup .py

---
+++ 1-1-CircleApp/1-1-CircleApp.py
@@ -0,0 +1,7 @@
+
+# add base class with _attribute to encapsulate #21
+# add @property decorator to enable getter #22
+# add @attribute.setter decorator to enable setter #23
+# test: getter method invocation . #24
+# test: setter method invocation = #25
+
